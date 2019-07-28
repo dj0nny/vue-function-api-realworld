9083e897410d473024523febcc10c6968bb24e72
dj0nny <frank96990@gmail.com>
2019-07-28 20:13:24 +0200

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue';
+import { plugin } from 'vue-function-api';
+import hooks from '@u3u/vue-hooks';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+
+Vue.use(plugin);
+Vue.use(hooks);
 
 Vue.config.productionTip = false;
 
