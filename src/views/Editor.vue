4750469256db4bfa40c2861a4d2e426222ac91d1
dj0nny <frank96990@gmail.com>
2019-08-13 21:20:22 +0200

    set up form for adding new article in Editor.vue

--- src/views/Editor.vue
+++ src/views/Editor.vue
@@ -4,21 +4,21 @@
       <div class="row">
 
         <div class="col-md-10 offset-md-1 col-xs-12">
-          <form>
+          <form @submit.prevent="addArticle">
             <fieldset>
               <fieldset class="form-group">
-                  <input type="text" class="form-control form-control-lg" placeholder="Article Title">
+                  <input v-model="newArticle.article.title" type="text" class="form-control form-control-lg" placeholder="Article Title">
               </fieldset>
               <fieldset class="form-group">
-                  <input type="text" class="form-control" placeholder="What's this article about?">
+                  <input v-model="newArticle.article.description" type="text" class="form-control" placeholder="What's this article about?">
               </fieldset>
               <fieldset class="form-group">
-                  <textarea class="form-control" rows="8" placeholder="Write your article (in markdown)"></textarea>
+                  <textarea v-model="newArticle.article.body" class="form-control" rows="8" placeholder="Write your article (in markdown)"></textarea>
               </fieldset>
               <fieldset class="form-group">
-                  <input type="text" class="form-control" placeholder="Enter tags"><div class="tag-list"></div>
+                  <input v-model="newArticle.article.tagList" type="text" class="form-control" placeholder="Enter tags"><div class="tag-list"></div>
               </fieldset>
-              <button class="btn btn-lg pull-xs-right btn-primary" type="button">
+              <button class="btn btn-lg pull-xs-right btn-primary" type="submit">
                   Publish Article
               </button>
             </fieldset>
@@ -31,7 +31,20 @@
 </template>
 
 <script>
+import { value } from 'vue-function-api';
+
 export default {
   name: 'Editor',
+  setup() {
+    const newArticle = value({ article: { title: '', description: '', body: '', tagList: [] } });
+
+    const addArticle = () => {
+    }
+
+    return {
+      newArticle,
+      addArticle,
+    }
+  }
 };
 </script>
